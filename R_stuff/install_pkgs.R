b66668371bb7728c82859980b5f1fb085b5aca77
Filippo Valle <fvalle@to.infn.it>
2020-01-15 12:16:08 +0100

    updates

---
+++ R_stuff/install_pkgs.R
@@ -0,0 +1,20 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager", dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+BiocManager::install("edgeR")
+BiocManager::install('DESeq')
+
+install.packages('statmod', dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages("gplots", dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages("fastICA", dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages("svglite", dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages("stringi", dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+BiocManager::install("WGCNA")
+BiocManager::install("GO.db")
+BiocManager::install("AnnotationDbi")
+BiocManager::install("org.Hs.eg.db")
+BiocManager::install("biomaRt")
+
+
+
