d358d8d4a5891783e47f7615b073e7fe2e350b20
Filippo Valle <filippo.valle@zoho.com>
2020-03-26 18:02:44 +0100

    add docker

---
+++ docker/requirements.jl
@@ -0,0 +1,10 @@
+using Pkg
+Pkg.add("Pandas")
+Pkg.add("DataFrames")
+Pkg.add("PyCall")
+Pkg.add("PyPlot")
+Pkg.add("CSV")
+Pkg.add("LsqFit")
+Pkg.add("Printf")
+Pkg.add("KernelDensity")
+Pkg.add("Distributions")
