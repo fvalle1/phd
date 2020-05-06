fbaee5527097b7f2931f2c973c80e701c6703551
Filippo Valle <filippo.valle@edu.unito.it>
2020-05-06 11:29:19 +0200

    fix WGCNA dependency
    foreign requires R>=4.0.0 from march 2020

--- R_stuff/install_pkgs.R
+++ R_stuff/install_pkgs.R
@@ -11,6 +11,9 @@ install.packages("svglite", dependencies=TRUE, repos='http://cran.rstudio.com/')
 install.packages("stringi", dependencies=TRUE, repos='http://cran.rstudio.com/')
 install.packages("clustree", dependencies=TRUE, repos='http://cran.rstudio.com/')
 
+require(devtools)
+install_version("foreign", version="0.8-55", dependencies=TRUE, repos='http://cran.rstudio.com/')
+
 BiocManager::install("WGCNA")
 BiocManager::install("GO.db")
 BiocManager::install("AnnotationDbi")
