fix normalisation

hsbmpy.py
@@ -411,7 +411,9 @@ def add_score_lines(ax, scores, V="V", labels=None, h=False, c=False, alpha=0.8,
         'hierarchical': 'darkgreen',
         'lda': 'violet',
         'RPPA Clusters': 'red',
-        'wgcna': 'purple'
+        'wgcna': 'purple',
+        "Subtype_Selected": "red",
+        "BRCA_Subtype_PAM50": "blue"
     }
 
     for label in labels:
@@ -608,6 +610,19 @@ def get_scores(directory, labels, df_files=None, algorithm='topsbm', verbose=Fal
             except:
                 print(*sys.exc_info())
                 print("Skipping level ", l)
+
+        # add the first point where all sample are in the same cluster by definition
+        if xl[0] < xl[-1]:
+            idx = 0
+        else:
+            idx = len(xl)
+        true_labels, _ = define_labels(get_cluster_given_l(l, directory, algorithm), df_files, label=label)
+        predicted_labels = np.ones_like(true_labels)
+        scores[label]['h'].insert(idx,metrics.cluster.homogeneity_score(true_labels, predicted_labels))
+        scores[label]['c'].insert(idx,metrics.cluster.completeness_score(true_labels, predicted_labels))
+        scores[label]['V'].insert(idx,metrics.cluster.v_measure_score(true_labels, predicted_labels))
+        xl.insert(idx,len(np.unique(predicted_labels)))
+
         scores[label]['xl'] = xl
     if len(labels) >= 2:
         h = np.array(scores[labels[0]]['h'])
@@ -657,6 +672,19 @@ def get_scores_shuffled(directory, df_files, algorithm='topsbm', label='primary_
     except:
         print(*sys.exc_info())
         print("shuffled files not found")
+
+    # add the first point where all sample are in the same cluster by definition
+    if xl[0] < xl[-1]:
+        idx = 0
+    else:
+        idx = len(xl)
+    true_labels, _ = define_labels(get_cluster_given_l(l, directory, algorithm), df_files, label=label)
+    predicted_labels = np.ones_like(true_labels)
+    scores['h'].insert(idx,metrics.cluster.homogeneity_score(true_labels, predicted_labels))
+    scores['c'].insert(idx,metrics.cluster.completeness_score(true_labels, predicted_labels))
+    scores['V'].insert(idx,metrics.cluster.v_measure_score(true_labels, predicted_labels))
+    xl.insert(idx,len(np.unique(predicted_labels)))
+
     scores['xl'] = xl
     return scores
 
@@ -793,10 +821,10 @@ def out_to_file(out, index, name='new_method', l=0):
 
 
 #normalise to hsbm
-def normalise_score(scores : dict, base_algorithm="hsbm", operation=lambda x,y: x/y)->None:
+def normalise_score(scores : dict, base_algorithm="hsbm", operation=lambda x,y: x/y, epsilon = 1e-6)->None:
     "save scaled data to scores[norm_V]"
-    for algorithm in scores.keys():
+    for algorithm in scores.keys(): #the first point is always constructed and np.interp wants sorted data so[:-1:-1]
         baseline = np.interp(scores[algorithm]["xl"],
-							 scores[base_algorithm]["xl"][::-1],
-							 scores[base_algorithm]["V"][::-1])
-        scores[algorithm]["norm_V"]=operation(np.array(scores[algorithm]["V"]),baseline)
+    					 scores[base_algorithm]["xl"][:-1][::-1],
+    					 scores[base_algorithm]["V"][:-1][::-1])
+        scores[algorithm]["norm_V"]=operation(np.array(scores[algorithm]["V"])+epsilon,baseline+epsilon)